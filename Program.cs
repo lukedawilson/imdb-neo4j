24c3dff1c71b9a6f6d361d8b56601c0c506db872
Luke Wilson <lukedawilson@users.noreply.github.com>
2020-12-26 16:31:46 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Imdb
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# imdb-neo4j
+
+Import of the [IMDB dataset](https://www.imdb.com/interfaces/) into the [Neo4j graph database](https://neo4j.com/).
